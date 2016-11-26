42477baec95c526c189595bae09376ad1103da7a
Ziv Zamechek <ziv921@yahoo.com>
2016-11-26 18:20:54 -0500

    Added routes controllers model and view for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,38 @@
 class EventsController < ApplicationController
+  def index  
+    @events = Event.all  
+    render json: @events 
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    render json: @event
+  end
+
+  def create  
+    @event = Event.new(event_params)  
+    if @event.save  
+      render json: @event
+    end  
+  end  
+
+  def update  
+    @event = Event.find(params[:id])  
+
+    if @event.update(event_params)  
+      render json: @event
+    end  
+  end  
+
+  def destroy  
+    @event = Event.find(params[:id])  
+    @event.destroy  
+    render json: @event
+  end  
+
+  private  
+
+  def event_params 
+    params.require(:event).permit(:name, :email, :phone, :job_title, :job_description, :location, :category)  
+  end  
 end
